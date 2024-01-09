7ed1134ffed87fb1cce06dd0642aed8b14f6a4ed
npt-1707 <npthanh132@gmail.com>
2024-01-09 21:54:48 +0700

    update

--- src/api.py
+++ src/api.py
@@ -9,22 +9,25 @@ CORS(app)
 csv_path = "/Users/nguyenphucthanh/Downloads/price_optimization/data/retail_price.csv"
 data = pd.read_csv(csv_path)
 
-@app.route("/api/ids", methods=["GET"])
-def get_ids():
-    ids = list(data["product_id"].unique())
-    return jsonify(ids)
-
-
-@app.route("/api/categories", methods=["GET"])
-def get_categories():
-    categories = list(data["product_category_name"].unique())
-    return jsonify(categories)
-
-
 @app.route("/api/ids/<category>", methods=["GET"])
-def get_ids_by_category(category):
-    ids = list(data[data["product_category_name"] == category]["product_id"].unique())
-    return jsonify(ids)
+def get_ids(category):
+    if category == "":
+        ids = list(data["product_id"].unique())
+        return jsonify(ids)
+    else:
+        ids = list(data[data["product_category_name"] == category]["product_id"].unique())
+        return jsonify(ids)
+
+
+@app.route("/api/categories/<id>", methods=["GET"])
+def get_categories(id):
+    if id == "":
+        categories = list(data["product_category_name"].unique())
+        return jsonify(categories)
+    else:
+        categories = list(data[data["product_id"] == id]["product_category_name"].unique())
+        return jsonify(categories)
+
 
 
 if __name__ == "__main__":
